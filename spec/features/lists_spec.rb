138aeb8c04d9022b158bc268d32fa435c5b80dcc
Alison Neer <alison.neer@gmail.com>
2014-04-01 19:31:39 -0400

    add spec for list

---
+++ spec/features/lists_spec.rb
@@ -0,0 +1,15 @@
+require 'spec_helper'
+
+feature "Todo Lists" do
+  feature "User views a list" do
+    given(:list) { create :list_with_items }
+
+    scenario do
+      visit list_path(list)
+
+      list.items.each do |item|
+        expect(page).to have_content(item.name)
+      end
+    end
+  end
+end
