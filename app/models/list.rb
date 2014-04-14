5dfd635ffa1bb6aa68766ed3ad8dc0a0a88f12aa
Alison Neer <alison.neer@gmail.com>
2014-04-13 21:23:46 -0400

    better add a thing berterns

--- app/models/list.rb
+++ app/models/list.rb
@@ -1,3 +1,11 @@
 class List < ActiveRecord::Base
   has_many :tasks , dependent: :delete_all
+
+  def pending_task_count
+    tasks.pending_count
+  end
+
+  def complete_task_count
+    tasks.complete_count
+  end
 end
--- app/models/task.rb
+++ app/models/task.rb
@@ -1,3 +1,20 @@
 class Task < ActiveRecord::Base
+  extend Enumerize
+
   belongs_to :list
+
+  scope :pending, -> { with_status :pending }
+  scope :complete, -> { with_status :complete }
+
+  enumerize :status, in: {pending: 1, complete: 2}, default: :pending, scope: true
+
+  class << self
+    def pending_count
+      pending.count
+    end
+
+    def complete_count
+      complete.count
+    end
+  end
 end
